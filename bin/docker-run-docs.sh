04602b2a5ba0e94335038af7246e42adcc1c0f1e
piemontez <rafaelpiemontez@gmail.com>
2022-10-26 09:32:18 -0300

    diagramas

--- bin/docker-run-docs.sh
+++ bin/docker-run-docs.sh
@@ -31,4 +31,6 @@ echo "Roda o container"
         -p $PORT:9000 \
         -v public:/var/www/docs/public:ro \
         --name iam_docs_prod \
-        -d iam_docs_prod
+        -it iam_docs_prod
+
+#docker run -p 9000:9000 -v public:/var/www/docs/public:ro --name iam_docs_prod -it iam_docs_prod
