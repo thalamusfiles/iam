4fc3de582960a20280a313a8be71c1a7ac85cdd9
piemontez <rafaelpiemontez@gmail.com>
2023-02-11 10:55:11 -0300

    primeiros testes regiao

--- backend/test/app/mgt/controller/region.controller.e2e-spec.ts
+++ backend/test/app/mgt/controller/region.controller.e2e-spec.ts
@@ -9,6 +9,16 @@ describe('RegionController (e2e)', () => {
   const reginUrl = '/mgt/region';
   let app: INestApplication;
 
+  let uuidRegion = null;
+  const regionToCreate: EntityProps<Region> = {
+    entity: {
+      initials: 'Global',
+      name: 'Global',
+      description: 'Região de aplicações com único servidor',
+    },
+    user: {},
+  };
+
   beforeAll(async () => {
     const moduleFixture = await Test.createTestingModule({
       imports: [AppModule],
@@ -19,19 +29,27 @@ describe('RegionController (e2e)', () => {
   });
 
   it(`${reginUrl}/ (Post) Cria nova região`, async () => {
-    const region: EntityProps<Region> = {
-      entity: {
-        initials: 'Global',
-        name: 'Global',
-        description: 'Região de aplicações com único servidor',
-      },
-      user: {},
-    };
-    const result = await request(app.getHttpServer()).post(reginUrl).send(region).expect(201);
+    const result = await request(app.getHttpServer()).post(reginUrl).send(regionToCreate).expect(201);
 
     expect(result.body.user).toBeDefined();
     expect(result.body.entity).toBeDefined();
     expect(result.body.entity.uuid).toBeTruthy();
+
+    uuidRegion = result.body.entity.uuid;
+  });
+
+  it(`${reginUrl}/ (Post) Tenta criar a mesma região (não pode)`, async () => {
+    await request(app.getHttpServer()).post(reginUrl).send(regionToCreate).expect(500);
+  });
+
+  it(`${reginUrl}/ (Put) Atualiza a região`, async () => {
+    const result = await request(app.getHttpServer()).put(`${reginUrl}/${uuidRegion}`).send(regionToCreate).expect(201);
+
+    expect(result.body.user).toBeDefined();
+    expect(result.body.entity).toBeDefined();
+    expect(result.body.entity.uuid).toBeTruthy();
+
+    uuidRegion = result.body.entity.uuid;
   });
 
   it(`${reginUrl}/ (Get) Busca as regões`, () => {
