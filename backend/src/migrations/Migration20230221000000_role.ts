211f1fc260b0e0ecf9ec4f5bc11de533f6ab1f2d
piemontez <rafaelpiemontez@gmail.com>
2023-03-01 08:11:23 -0300

    inicio cadastro de Permissions

---
+++ backend/src/migrations/Migration20230221000000_role.ts
@@ -0,0 +1,65 @@
+import { Migration } from '@mikro-orm/migrations';
+
+export class Migration20230228113920 extends Migration {
+  async up(): Promise<void> {
+    this.addSql(
+      `create table "role" (
+        "uuid" uuid not null default uuid_generate_v4(), 
+        "created_at" timestamptz(0) not null, 
+        "updated_at" timestamptz(0) not null, 
+        "created_by_uuid" uuid not null, 
+        "updated_by_uuid" uuid not null, 
+        "deleted_at" timestamptz(0) null, 
+        "deleted_by_uuid" uuid null, 
+        "initials" varchar(255) not null, 
+        "name" varchar(255) not null, 
+        "description" varchar(255) not null, 
+        "application_uuid" uuid not null, 
+        
+        constraint "role_pkey" primary key ("uuid"), 
+        constraint role_initials_check check (LENGTH(initials) >= 3), 
+        constraint role_name_check check (LENGTH(name) >= 3), 
+        constraint role_description_check check (LENGTH(description) >= 10)
+      );`,
+    );
+
+    this.addSql('alter table "role" add constraint "role_application_uuid_initials_unique" unique ("application_uuid", "initials");');
+
+    this.addSql(
+      `create table "user_role" (
+        "user_uuid" uuid not null, 
+        "role_uuid" uuid not null, 
+        
+        constraint "user_role_pkey" primary key ("user_uuid", "role_uuid")
+      );`,
+    );
+
+    this.addSql(
+      'alter table "role" add constraint "role_created_by_uuid_foreign" foreign key ("created_by_uuid") references "user" ("uuid") on update cascade;',
+    );
+    this.addSql(
+      'alter table "role" add constraint "role_updated_by_uuid_foreign" foreign key ("updated_by_uuid") references "user" ("uuid") on update cascade;',
+    );
+    this.addSql(
+      'alter table "role" add constraint "role_deleted_by_uuid_foreign" foreign key ("deleted_by_uuid") references "user" ("uuid") on update cascade on delete set null;',
+    );
+    this.addSql(
+      'alter table "role" add constraint "role_application_uuid_foreign" foreign key ("application_uuid") references "system"."application" ("uuid") on update cascade on delete set null;',
+    );
+
+    this.addSql(
+      'alter table "user_role" add constraint "user_role_user_uuid_foreign" foreign key ("user_uuid") references "user" ("uuid") on update cascade on delete cascade;',
+    );
+    this.addSql(
+      'alter table "user_role" add constraint "user_role_role_uuid_foreign" foreign key ("role_uuid") references "role" ("uuid") on update cascade on delete cascade;',
+    );
+  }
+
+  async down(): Promise<void> {
+    this.addSql('alter table "user_role" drop constraint "user_role_role_uuid_foreign";');
+
+    this.addSql('drop table if exists "role" cascade;');
+
+    this.addSql('drop table if exists "user_role" cascade;');
+  }
+}
--- backend/src/model/Permission.ts
+++ backend/src/model/Permission.ts
@@ -1,19 +1,20 @@
 import { Check, Collection, Entity, ManyToMany, ManyToOne, Property, Unique } from '@mikro-orm/core';
 import { IamBaseEntityWithDelete } from './Base/IamBaseEntityWithDelete';
-import { Permission } from './Permission';
+import { Role } from './Role';
 import { Application } from './System/Application';
-import { User } from './User';
 
 @Entity()
 @Unique({ properties: ['application', 'initials'] })
-export class Role extends IamBaseEntityWithDelete {
+export class Permission extends IamBaseEntityWithDelete {
   @Check({ expression: 'LENGTH(initials) >= 3' })
   @Property({ nullable: false })
   initials!: string;
 
-  @Check({ expression: 'LENGTH(name) >= 3' })
   @Property({ nullable: false })
-  name!: string;
+  on!: string;
+
+  @Property({ nullable: false })
+  action!: string;
 
   @Check({ expression: 'LENGTH(description) >= 10' })
   @Property({ nullable: false })
@@ -22,9 +23,6 @@ export class Role extends IamBaseEntityWithDelete {
   @ManyToOne(() => Application, { nullable: false })
   application?: Application;
 
-  @ManyToMany(() => User, (user) => user.roles)
-  users = new Collection<User>(this);
-
-  @ManyToMany(() => Permission, 'roles', { owner: true, pivotTable: 'role_permission' })
-  permissions = new Collection<Permission>(this);
+  @ManyToMany(() => Role, (role) => role.permissions)
+  roles = new Collection<Role>(this);
 }
