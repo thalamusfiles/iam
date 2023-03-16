7b84e4a07ed6946692ef7add2d8a9e314f7553f0
piemontez <rafaelpiemontez@gmail.com>
2023-03-16 18:09:54 -0300

    inicio migração para react hook

--- frontend_web/src/views/public/login/index.tsx
+++ frontend_web/src/views/public/login/index.tsx
@@ -37,7 +37,7 @@ function LoginPage() {
     setForm({ password: e.target.value, username: form.username });
   }
 
-  function login() {
+  function toLogin() {
     UserContext.login(form.username, form.password).catch((error) => {
       setErros({
         username: 'User not found',
@@ -46,14 +46,14 @@ function LoginPage() {
     });
   }
 
-  function onKeyUpFilter(e: any) {
-    if (e.charCode === 13) {
-      login();
-    }
+  function toRegister() {
+    historyPush('register', { region, app });
   }
 
-  function register() {
-    historyPush('register', { region, app });
+  function onKeyUpFilter(e: any) {
+    if (e.charCode === 13) {
+      toLogin();
+    }
   }
 
   return (
@@ -119,13 +119,13 @@ function LoginPage() {
                 <Card.Footer>
                   <Row>
                     <Col sm={4}>
-                      <Button variant="ligth" onClick={register} block>
+                      <Button variant="ligth" onClick={toRegister} block>
                         {__('login.action.register')}
                       </Button>
                     </Col>
                     <Col></Col>
                     <Col sm={4}>
-                      <Button variant="primary" onClick={login} block>
+                      <Button variant="primary" onClick={toLogin} block>
                         <FontAwesomeIcon icon={IconsDef.login} /> {__('login.action.login')}
                       </Button>
                     </Col>
@@ -139,143 +139,4 @@ function LoginPage() {
     </div>
   );
 }
-
-/*
-@WMSI18N()
-class LoginPage extends React.Component<{ __: Function; match: any }> {
-  state = {
-    erros: {
-      username: null,
-      password: null,
-    },
-    redirectTo: null as null | string,
-    username: '',
-    password: '',
-  };
-
-  constructor(props: any) {
-    super(props);
-
-    const urlSearch = historySearch();
-    this.state.redirectTo = urlSearch.redirectTo as string;
-  }
-
-  onKeyUpFilter = (e: any) => {
-    if (e.charCode === 13) {
-      this.login();
-    }
-  };
-
-  handleUsername = (e: any) => {
-    this.setState({ username: e.target.value });
-  };
-
-  handlePassword = (e: any) => {
-    this.setState({ password: e.target.value });
-  };
-
-  login = () => {
-    UserContext.login(this.state.username, this.state.password).catch((error) => {
-      this.setState({
-        erros: {
-          username: 'User not found',
-          password: 'Invalid pass',
-        },
-      });
-    });
-  };
-
-  register = () => {
-    const { region, app } = useParams();
-    historyPush('register', { region, app });
-  };
-
-  render() {
-    const { __ } = this.props;
-    const { redirectTo, erros } = this.state;
-
-    return (
-      <div style={{ backgroundImage: `url(${bgImg})` }} className="bgImageCover">
-        <div style={{ backgroundColor: 'rgba(90,90,90,.8)', width: '100%', padding: 20 }} className="bgImageCover">
-          <Row>
-            <Col md={{ span: 4, offset: 4 }} sm={{ span: 6, offset: 3 }} className="text-center text-white mb-5 mt-5">
-              <h2>{__('login.title')}</h2>
-              <h4>{__('login.subtitle')}</h4>
-            </Col>
-            <Col md={{ span: 4, offset: 4 }} sm={{ span: 6, offset: 3 }}>
-              <Col xs={{ span: 10, offset: 1 }}>
-                <Card id="login_card" border="info" className="cdShadow">
-                  <Card.Body>
-                    <p>
-                      <img src="/logo.png" alt="logo" id="logo" />
-                      {__('login.cardindo')}
-                    </p>
-
-                    {!redirectTo && (
-                      <Row>
-                        <Col>
-                          <Form.Group controlId="validationCustom01">
-                            <Form.Label>{__('login.region')}</Form.Label>
-                            <Form.Control type="text" value="Global System" disabled />
-                          </Form.Group>
-                        </Col>
-                        <Col>
-                          <Form.Group controlId="validationCustom01">
-                            <Form.Label>{__('login.system')}</Form.Label>
-                            <Form.Control type="text" value="Root" disabled />
-                          </Form.Group>
-                        </Col>
-                      </Row>
-                    )}
-
-                    <Form.Group controlId="validationCustom01">
-                      <Form.Label>{__('login.username')}</Form.Label>
-                      <Form.Control
-                        placeholder="Enter your user name, code or email"
-                        type="email"
-                        onKeyPress={this.onKeyUpFilter}
-                        onChange={this.handleUsername}
-                        isInvalid={!!erros.username}
-                      />
-                      <Form.Control.Feedback type="invalid">{erros.username}</Form.Control.Feedback>
-                    </Form.Group>
-
-                    <Form.Group>
-                      <Form.Label>{__('login.password')}</Form.Label>
-                      <Form.Control
-                        placeholder={__('login.password') as string}
-                        type="password"
-                        autoComplete="off"
-                        onKeyPress={this.onKeyUpFilter}
-                        onChange={this.handlePassword}
-                        isInvalid={!!erros.password}
-                      />
-                      <Form.Control.Feedback type="invalid">{erros.password}</Form.Control.Feedback>
-                    </Form.Group>
-                    <Card.Link color="secondary">{__('login.action.forgotpass')}</Card.Link>
-                  </Card.Body>
-                  <Card.Footer>
-                    <Row>
-                      <Col sm={4}>
-                        <Button variant="ligth" onClick={this.register} block>
-                          {__('login.action.register')}
-                        </Button>
-                      </Col>
-                      <Col></Col>
-                      <Col sm={4}>
-                        <Button variant="primary" onClick={this.login} block>
-                          <FontAwesomeIcon icon={IconsDef.login} /> {__('login.action.login')}
-                        </Button>
-                      </Col>
-                    </Row>
-                  </Card.Footer>
-                </Card>
-              </Col>
-            </Col>
-          </Row>
-        </div>
-      </div>
-    );
-  }
-}*/
 export default LoginPage;
--- frontend_web/src/views/public/register/index.tsx
+++ frontend_web/src/views/public/register/index.tsx
@@ -1,20 +1,152 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import React from 'react';
+import { useEffect, useState } from 'react';
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
 import Col from 'react-bootstrap/Col';
 import Form from 'react-bootstrap/Form';
 import Row from 'react-bootstrap/Row';
+import { useParams, useSearchParams } from 'react-router-dom';
 import bgRotate01 from '../../../assets/bg_rotate_01.jpeg';
 import bgRotate02 from '../../../assets/bg_rotate_02.jpeg';
 import bgRotate03 from '../../../assets/bg_rotate_03.jpeg';
 import bgRotate04 from '../../../assets/bg_rotate_04.jpeg';
 import { IconsDef } from '../../../commons/consts';
-import { WMSI18N } from '../../../commons/i18';
-import { historyPush, historySearch } from '../../../commons/route';
+import { useI18N } from '../../../commons/i18';
+import { historyPush } from '../../../commons/route';
 
 const bgImg = [bgRotate01, bgRotate02, bgRotate03, bgRotate04][Math.floor(Math.random() * 4)];
 
+function RegisterPage() {
+  const __ = useI18N();
+  const { region, app } = useParams();
+  const [form, setForm] = useState({ username: '', password: '' });
+  const [erros, setErros] = useState({ username: null as string | null, password: null as string | null });
+  const [redirectTo, setRedirectTo] = useState(null as string | null);
+  const [searchParams] = useSearchParams();
+
+  useEffect(() => {
+    setRedirectTo(searchParams.get('redirectTo'));
+  }, [searchParams]);
+
+  function handleUsername(e: any) {
+    setForm({ username: e.target.value, password: form.password });
+  }
+
+  function handlePassword(e: any) {
+    setForm({ password: e.target.value, username: form.username });
+  }
+
+  function toLogin() {
+    historyPush('login', { region, app });
+  }
+
+  function toRegister() {
+    historyPush('home_account');
+  }
+
+  function onKeyUpFilter(e: any) {
+    if (e.charCode === 13) {
+      toLogin();
+    }
+  }
+
+  return (
+    <div style={{ backgroundImage: `url(${bgImg})` }} className="bgImageCover">
+      <div style={{ backgroundColor: 'rgba(90,90,90,.8)', width: '100%', padding: 20 }} className="bgImageCover">
+        <Row>
+          <Col md={{ span: 4, offset: 4 }} sm={{ span: 6, offset: 3 }} className="text-center text-white mb-5 mt-5">
+            <h2>{__('register.title')}</h2>
+            <h4>{__('register.subtitle')}</h4>
+          </Col>
+          <Col md={{ span: 4, offset: 4 }} sm={{ span: 6, offset: 3 }}>
+            <Col xs={{ span: 10, offset: 1 }}>
+              <Card id="login_card" border="info" className="cdShadow">
+                <Card.Body>
+                  <p>
+                    <img src="/logo.png" alt="logo" id="logo" />
+                    {__('register.cardindo')}
+                  </p>
+
+                  <Form.Group controlId="validationCustom01">
+                    <Form.Label>{__('register.username')}</Form.Label>
+                    <Form.Control
+                      placeholder="Enter your user name, code or email"
+                      type="username"
+                      onKeyPress={onKeyUpFilter}
+                      onChange={handleUsername}
+                      isInvalid={!!erros.username}
+                    />
+                    <Form.Control.Feedback type="invalid">{erros.username}</Form.Control.Feedback>
+                  </Form.Group>
+
+                  <Form.Group controlId="validationCustom01">
+                    <Form.Label>{__('register.email')}</Form.Label>
+                    <Form.Control
+                      placeholder="Enter your email"
+                      type="email"
+                      onKeyPress={onKeyUpFilter}
+                      onChange={handleUsername}
+                      isInvalid={!!erros.username}
+                    />
+                    <Form.Control.Feedback type="invalid">{erros.username}</Form.Control.Feedback>
+                  </Form.Group>
+
+                  <Row>
+                    <Col>
+                      <Form.Group>
+                        <Form.Label>{__('register.password')}</Form.Label>
+                        <Form.Control
+                          placeholder={__('register.password') as string}
+                          type="password"
+                          autoComplete="off"
+                          onKeyPress={onKeyUpFilter}
+                          onChange={handlePassword}
+                          isInvalid={!!erros.password}
+                        />
+                        <Form.Control.Feedback type="invalid">{erros.password}</Form.Control.Feedback>
+                      </Form.Group>
+                    </Col>
+                    <Col>
+                      <Form.Group>
+                        <Form.Label>{__('register.passwordcheck')}</Form.Label>
+                        <Form.Control
+                          placeholder={__('register.passwordcheck') as string}
+                          type="password"
+                          autoComplete="off"
+                          onKeyPress={onKeyUpFilter}
+                          onChange={handlePassword}
+                          isInvalid={!!erros.password}
+                        />
+                        <Form.Control.Feedback type="invalid">{erros.password}</Form.Control.Feedback>
+                      </Form.Group>
+                    </Col>
+                  </Row>
+                </Card.Body>
+                <Card.Footer>
+                  <Row>
+                    <Col sm={6}>
+                      <Button size="sm" variant="ligth" onClick={toLogin} block>
+                        {__('register.action.tologin')}
+                      </Button>
+                    </Col>
+                    <Col></Col>
+                    <Col sm={5}>
+                      <Button variant="primary" onClick={toRegister} block>
+                        <FontAwesomeIcon icon={IconsDef.save} /> {__('login.action.register')}
+                      </Button>
+                    </Col>
+                  </Row>
+                </Card.Footer>
+              </Card>
+            </Col>
+          </Col>
+        </Row>
+      </div>
+    </div>
+  );
+}
+
+/*
 @WMSI18N()
 class RegisterPage extends React.Component<{ __: Function; match: any }> {
   state = {
@@ -157,4 +289,5 @@ class RegisterPage extends React.Component<{ __: Function; match: any }> {
     );
   }
 }
+*/
 export default RegisterPage;
