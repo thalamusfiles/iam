a35c7428fabd6d65915c71e1b4df3a63e1294570
piemontez <rafaelpiemontez@gmail.com>
2023-03-20 10:32:22 -0300

    refactor edits

--- frontend_web/src/commons/i18.ts
+++ frontend_web/src/commons/i18.ts
@@ -3,11 +3,9 @@ import ptBR from 'date-fns/locale/pt-BR';
 import i18next from 'i18next';
 import LanguageDetector from 'i18next-browser-languagedetector';
 import Backend from 'i18next-http-backend';
-import React from 'react';
 import { registerLocale } from 'react-datepicker';
 import { initReactI18next } from 'react-i18next';
 import env from '../.env';
-import { IReactComponent, IWrappedComponent } from './types/IReactComponent';
 
 i18next
   .use(Backend)
@@ -28,33 +26,6 @@ registerLocale('en-US', enUS);
 
 //Função de tradução
 const __ = (a: any, options?: any) => i18next.t(a, options);
-
-/**
- * Injeta i18n como propriedade da classe ou hook
- * @param stores
- */
-function injectI18N(component: IReactComponent<any>): IReactComponent<any> {
-  const Injector: IReactComponent<any> = React.forwardRef((props, ref) => {
-    const newProps = { ...props };
-
-    if (ref) {
-      newProps.ref = ref;
-    }
-    newProps.__ = __;
-
-    return React.createElement(component, newProps);
-  });
-  return Injector;
-}
-
-/**
- * Decorator para adicionar i18n na classe ou hook
- * @param stores
- */
-export function WMSI18N(): <T extends IReactComponent<any>>(target: T) => T & (T extends IReactComponent<infer P> ? IWrappedComponent<P> : never);
-export function WMSI18N() {
-  return (componentClass: React.ComponentClass<any, any>) => injectI18N(componentClass);
-}
 
 export const useI18N = () => __;
 
--- frontend_web/src/views/account/index.tsx
+++ frontend_web/src/views/account/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-import { WMSI18N } from '../../commons/i18';
+import { useI18N } from '../../commons/i18';
 import Footer from '../../components/Footer';
 import NotificationMenu from '../../components/Notification';
 import UserCtxInstance from '../../store/userContext';
@@ -23,22 +23,20 @@ const Account: React.FC = () => {
   );
 };
 
-@WMSI18N()
-class HeaderAccount extends React.Component<{ __?: Function }> {
-  render() {
-    const { __ } = this.props;
-    return (
-      <Navbar className="header">
-        <Navbar.Brand className="mr-auto">
-          <img src="/logo.png" alt="logo" />
-          {__!('menu.brand')} - {__!('login.subtitle')}
-        </Navbar.Brand>
-        <Nav>
-          <Nav.Item onClick={() => UserCtxInstance.logout()}>{__!('menu.logout')}</Nav.Item>
-        </Nav>
-      </Navbar>
-    );
-  }
-}
+const HeaderAccount: React.FC = () => {
+  const __ = useI18N();
+
+  return (
+    <Navbar className="header">
+      <Navbar.Brand className="mr-auto">
+        <img src="/logo.png" alt="logo" />
+        {__!('menu.brand')} - {__!('login.subtitle')}
+      </Navbar.Brand>
+      <Nav>
+        <Nav.Item onClick={() => UserCtxInstance.logout()}>{__!('menu.logout')}</Nav.Item>
+      </Nav>
+    </Navbar>
+  );
+};
 
 export default Account;
